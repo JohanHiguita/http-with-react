a86e0a6d986db26c46daf357ae483c648659d5f2
johan <higuitajohan@gmail.com>
2020-10-11 20:15:08 -0500

    Handling errors locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,18 +5,20 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(resp => {
-                const posts = resp.data.slice(0,4);
+                const posts = resp.data.slice(0, 4);
                 const updatedPosts = posts.map(p => {
                     return {
                         ...p,
@@ -24,29 +26,37 @@ class Blog extends Component {
                     }
                 })
                 this.setState({ posts: updatedPosts })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ error: true });
             });
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
+        this.setState({ selectedPostId: id })
     }
     render() {
-        const posts = this.state.posts.map(post => {
-
-            return <Post 
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}    
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+    
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
                 />
-        });
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost id={this.state.selectedPostId}/>
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
