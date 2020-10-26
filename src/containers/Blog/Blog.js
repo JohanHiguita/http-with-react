541935d9bb4ec9a6ffa3e2fed2aa7816b3d07f33
johan <higuitajohan@gmail.com>
2020-10-25 22:46:29 -0500

    189. Setting up links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -42,7 +42,7 @@ class Blog extends Component {
         let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-    
+
                 return <Post
                     key={post.id}
                     title={post.title}
@@ -53,7 +53,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
